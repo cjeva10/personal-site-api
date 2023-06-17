1bdff83487b37815f45137d0b704cfeb4ff6da6a
cjeva10 <cjevanko@gmail.com>
2023-06-17 15:38:47 -0400

    protected post/put/delete routes on assets

--- internal/routes/asset/asset.go
+++ internal/routes/asset/asset.go
@@ -2,19 +2,22 @@ package assetRoutes
 
 import (
 	assetHandler "git.chrisevanko.com/personal-site-api.git/internal/handlers/asset"
+	"git.chrisevanko.com/personal-site-api.git/internal/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupAssetRoutes(router fiber.Router) {
-    asset := router.Group("/asset")
+	asset := router.Group("/asset")
 
-    asset.Post("/", assetHandler.CreateAssets)
+	asset.Get("/", assetHandler.GetAssets)
+	asset.Get("/:assetSymbol", assetHandler.GetAsset)
 
-    asset.Get("/", assetHandler.GetAssets)
-    
-    asset.Get("/:assetSymbol", assetHandler.GetAsset)
+    // authenticate all routes that can change the db
+	asset.Use(middleware.Middleware)
 
-    asset.Put("/:assetId", assetHandler.UpdateAsset)
+	asset.Post("/", assetHandler.CreateAssets)
 
-    asset.Delete("/:assetId", assetHandler.DeleteAsset)
+	asset.Put("/:assetId", assetHandler.UpdateAsset)
+
+	asset.Delete("/:assetId", assetHandler.DeleteAsset)
 }
