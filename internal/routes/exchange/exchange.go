e36d6d6edaf658722b92fa46abf27d312a2b2e7c
cjeva10 <cjevanko@gmail.com>
2023-06-17 15:37:07 -0400

    protected post/put/delete routes in /exchange

--- internal/routes/exchange/exchange.go
+++ internal/routes/exchange/exchange.go
@@ -1,18 +1,20 @@
 package assetRoutes
 
 import (
-    "github.com/gofiber/fiber/v2"
-    exchangeHandler "git.chrisevanko.com/personal-site-api.git/internal/handlers/exchange"
+    "git.chrisevanko.com/personal-site-api.git/internal/middleware"
+	exchangeHandler "git.chrisevanko.com/personal-site-api.git/internal/handlers/exchange"
+	"github.com/gofiber/fiber/v2"
 )
 
 func SetupExchangeRoutes(router fiber.Router) {
     exchange := router.Group("/exchange")
 
-    exchange.Post("/", exchangeHandler.CreateExchanges)
-
     exchange.Get("/", exchangeHandler.GetExchanges)
     
     exchange.Get("/:exchangeSymbol", exchangeHandler.GetExchange)
+
+    exchange.Use(middleware.Middleware)
+    exchange.Post("/", exchangeHandler.CreateExchanges)
 
     exchange.Put("/:exchangeId", exchangeHandler.UpdateExchange)
 
